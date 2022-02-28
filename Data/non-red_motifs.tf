91878be80759dc34cee6e89070992f1a67ab07fa
daianna21 <daianna2121@outlook.es>
2022-02-27 18:21:11 -0600

    Add data used

---
+++ Data/non-red_motifs.tf
@@ -0,0 +1,63 @@
+AC  cluster_1
+XX
+ID  cluster_1
+XX
+DE  srgscymbrsyGCCmyCTrGTGGcmrcaggkg
+P0           a         c         g         t
+1           72       109       102        57
+2          194        98       286       136
+3          355       598       937       511
+4          557      1180       920       777
+5         2394      5815      2592      2651
+6         3612      4287      3894      4855
+7         7315      7213      5326      3677
+8         1950      7955      6128      8406
+9        12841      3936     11377      3624
+10        1847     10748     16373      2810
+11        1017     16467      1299     12996
+12        1495       530     28042      1712
+13        1454     28531       735      1060
+14         311     30589       449       431
+15       11568     18266       944      1003
+16         474     17710       666     12931
+17         306     30609       416       450
+18        2286      1378       848     27270
+19       12493      2217     14197      2875
+20         977      7294     22192      1320
+21        2471      3529      2052     23731
+22         456       649     26630       539
+23         968       967     20287      1356
+24        3264     12579      2042      5693
+25        6638      8220      4302      4078
+26        6377      2742      6075      1710
+27        1722      2869      1514      1648
+28        1888      1048      1133      1130
+29         718       687      1099       796
+30         421       513       803       506
+31          38        66       111       122
+32          54        44       218        21
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_1
+CC  AC: cluster_1
+CC  id: cluster_1
+CC  name: cluster_1
+CC  version: 
+CC  name: cluster_1
+CC  description: srgscymbrsyGCCmyCTrGTGGcmrcaggkg
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: cggcctatagcGCCccCTgGTGGccacaggtg
+CC  consensus.strict.rc: CACCTGTGGCCACCAGGGGGCGCTATAGGCCG
+CC  consensus.IUPAC: srgscymbrsyGCCmyCTrGTGGcmrcaggkg
+CC  consensus.IUPAC.rc: CMCCTGYKGCCACYAGRKGGCRSYVKRGSCYS
+CC  consensus.regexp: [cg][ag]g[cg]c[ct][ac][cgt][ag][cg][ct]GCC[ac][ct]CT[ag]GTGGc[ac][ag]cagg[gt]g
+CC  consensus.regexp.rc: C[AC]CCTG[CT][GT]GCCAC[CT]AG[AG][GT]GGC[AG][CG][CT][ACG][GT][AG]G[CG]C[CT][CG]
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 26
+CC  merged_AC: 'peak-motifs_results:::positions_6nt_m2,peak-motifs_results:::oligos_6nt_mkv3_m2,peak-motifs_results:::positions_6nt_m5,peak-motifs_results:::oligos_6nt_mkv3_m3,peak-motifs_results:::oligos_7nt_mkv4_m3,peak-motifs_results:::positions_6nt_m3,peak-motifs_results:::positions_7nt_m2,peak-motifs_results:::positions_7nt_m5,ctcf_ref_motif:::MA0139.1,peak-motifs_results:::oligos_7nt_mkv4_m5,peak-motifs_results:::positions_7nt_m1,peak-motifs_results:::oligos_6nt_mkv3_m1,peak-motifs_results:::oligos_6nt_mkv3_m4,peak-motifs_results:::oligos_7nt_mkv4_m4,peak-motifs_results:::oligos_7nt_mkv4_m1,peak-motifs_results:::oligos_7nt_mkv4_m2,peak-motifs_results:::dyads_m2,peak-motifs_results:::oligos_6nt_mkv3_m5,peak-motifs_results:::dyads_m3,peak-motifs_results:::positions_6nt_m1,peak-motifs_results:::dyads_m5,peak-motifs_results:::positions_7nt_m4,peak-motifs_results:::positions_6nt_m4,peak-motifs_results:::dyads_m4,peak-motifs_results:::positions_7nt_m3,peak-motifs_results:::dyads_m1'
+CC  merged_ID: 'positions_6nt_m2positions_6nt_m2,oligos_6nt_mkv3_m2oligos_6nt_mkv3_m2,positions_6nt_m5positions_6nt_m5,oligos_6nt_mkv3_m3oligos_6nt_mkv3_m3,oligos_7nt_mkv4_m3oligos_7nt_mkv4_m3,positions_6nt_m3positions_6nt_m3,positions_7nt_m2positions_7nt_m2,positions_7nt_m5positions_7nt_m5,CTCF,oligos_7nt_mkv4_m5oligos_7nt_mkv4_m5,positions_7nt_m1positions_7nt_m1,oligos_6nt_mkv3_m1oligos_6nt_mkv3_m1,oligos_6nt_mkv3_m4oligos_6nt_mkv3_m4,oligos_7nt_mkv4_m4oligos_7nt_mkv4_m4,oligos_7nt_mkv4_m1oligos_7nt_mkv4_m1,oligos_7nt_mkv4_m2oligos_7nt_mkv4_m2,dyads_m2dyads_m2,oligos_6nt_mkv3_m5oligos_6nt_mkv3_m5,dyads_m3dyads_m3,positions_6nt_m1positions_6nt_m1,dyads_m5dyads_m5,positions_7nt_m4positions_7nt_m4,positions_6nt_m4positions_6nt_m4,dyads_m4dyads_m4,positions_7nt_m3positions_7nt_m3,dyads_m1dyads_m1'
+XX
+//
